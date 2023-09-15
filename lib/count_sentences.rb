9eea4da7c70e35057e684c05dd4fe9935a22ff3e
SeanQuinn <Seanquinn917@gmail.com>
2023-09-15 11:06:59 -0400

    commit tree 95ae7d4410ee19276e231763b56d7c545d4600a0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false 
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false 
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]+/)
+    return new_array.count
+   
   end
 end
